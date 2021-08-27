d04a9cb26050c0b0686014f4563b106ed6e25923
SpringIntoJava <springintojava@gmail.com>
2021-08-27 11:55:43 +0300

    started 298

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,51 +1,60 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { catchError } from 'rxjs/operators';
 import { throwError } from 'rxjs';
 
 export interface AuthResponseData{
-    kind: string, 
-    idToken: string, 
+    kind: string,
+    idToken: string,
     email: string,
     refreshToken: string,
-    expiresIn: string, 
+    expiresIn: string,
     localId: string,
     registered?: boolean
 }
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class AuthService {
 
-  constructor(private http: HttpClient) { }
+    constructor(private http: HttpClient) { }
 
-  signup(email: string, password: string){
-      return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBg2Us_msHQViNA-SIcDDvRlt3M2RBPRpA', 
-      {
-        email: email,
-        password: password,
-        returnSecureToken: true
-      }).pipe(catchError(errorRes => {
+    signup(email: string, password: string){
+        return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBg2Us_msHQViNA-SIcDDvRlt3M2RBPRpA',
+            {
+                email: email,
+                password: password,
+                returnSecureToken: true
+            }).pipe(catchError(this.handleError));
+    }
+
+    login(email: string, password: string) {
+        return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBg2Us_msHQViNA-SIcDDvRlt3M2RBPRpA',
+            {
+                email: email,
+                password: password,
+                returnSecureToken: true
+            }).pipe(catchError(this.handleError));
+    }
+
+    private handleError(errorRes: HttpErrorResponse) {
         let errorMessage = 'An error occurred.';
         console.log(errorRes);
-        if(!errorRes.error || !errorRes.error.error){
+        if (!errorRes.error || !errorRes.error.error) {
             return throwError(errorMessage);
         }
-        switch(errorRes.error.error.message){
+        switch (errorRes.error.error.message) {
             case 'EMAIL_EXISTS':
                 errorMessage = 'This email exists already.';
+                break;
+            case 'EMAIL_NOT_FOUND':
+                errorMessage = 'This email does not exist';
+                break;
+            case 'INVALID_PASSWORD':
+                errorMessage = 'The password is incorrect';
+                break;
         }
         return throwError(errorMessage);
-      }));
-  }
-
-  login(email: string, password: string){
-      return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBg2Us_msHQViNA-SIcDDvRlt3M2RBPRpA', 
-      {
-        email: email,
-        password: password,
-        returnSecureToken: true
-      });
-  }
+    }
 }
